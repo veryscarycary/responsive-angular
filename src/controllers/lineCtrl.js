93064bc7fc48cc42782ea76128d165e0cd8ec8cf
Cary <carymeskell@gmail.com>
2017-01-08 21:38:55 -0800

    Change chart series to movie 1 and movie 2

--- src/controllers/lineCtrl.js
+++ src/controllers/lineCtrl.js
@@ -1,6 +1,6 @@
 app.controller('lineCtrl', function($scope, sales) {
   $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
+  $scope.series = ['Movie 1', 'Movie 2'];
   $scope.data = [
     [65, 59, 80, 81, 56, 55, 40],
     [28, 48, 40, 19, 86, 27, 90]
